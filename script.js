82b250ba813accaa1886053663fe3e86e3fd0e06
Andrea Simoes Herdade <andrea.simoes.herdade@gmail.com>
2024-11-17 17:47:16 -0300

    criando projeto

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("entrei no arquivo js")
+
+function conexaoFeita() {
+    console.log("Conexão feita com sucesso!");
+  }
+  
+  // Chama a função três vezes
+  conexaoFeita();
+  conexaoFeita();
+  conexaoFeita();
